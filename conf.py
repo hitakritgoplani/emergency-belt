26cddf3bdcca18768c31741385e75a7b805e729b
Hitakrit Goplani <hitakritgoplani@gmail.com>
2023-01-21 09:41:48 +0530

    Corrections

--- conf.py
+++ conf.py
@@ -1,17 +1,14 @@
-SID = 'AC3eb968b77e02754845c563ae7dfa8cf6'
-AUTH_TOKEN = 'e90d99da5323b11dc663e99119306c3f'
-FROM_NUMBER = '+12609605316'
-TO_NUMBER = '+919136505759' 
-API_KEY = 'ed9e7d76-a136-4815-b94e-56670e12600b'
-DEVICE_ID = 'BOLT5436637'
-LOCATION_API_KEY = 'Aq1PaVKIVfpAnqRzhTp6REdEyjU95KKyZOeDWTEj7kyaAJ8wVRmMKFNQy6bDl0GV'
+SID = 'Your Twilio SID'
+AUTH_TOKEN = 'Your Twilio AUTH_TOKEN'
+FROM_NUMBER = 'Your Twilio FROM_NUMBER'
+TO_NUMBER = 'Your Twilio TO_NUMBER' 
+API_KEY = 'Your Bolt API_KEY'
+DEVICE_ID = 'Your Bolt DEVICE_ID'
+LOCATION_API_KEY = 'Your Bing Map API'
 
 firebaseConfig = {
-  "apiKey": "AIzaSyBsQfOYXQT7lUycwgk9-xKyppfIJ2xKzSA",
-  "authDomain": "emergency-belt.firebaseapp.com",
-  "databaseURL": "https://emergency-belt-default-rtdb.firebaseio.com",
-  "projectId": "emergency-belt",
-  "storageBucket": "emergency-belt.appspot.com",
-  "messagingSenderId": "659318588814",
-  "appId": "1:659318588814:web:e9061134a77e2ae93350df"
-};
+  "apiKey": "apiKey",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://databaseName.firebaseio.com",
+  "storageBucket": "projectId.appspot.com"
+}
